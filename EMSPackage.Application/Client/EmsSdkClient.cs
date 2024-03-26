203db8bb365fc1b48580da3b192b0614a7a0a147
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 21:22:14 +0100

    Renamed method name

--- EMSPackage.Application/Client/EmsSdkClient.cs
+++ EMSPackage.Application/Client/EmsSdkClient.cs
@@ -8,7 +8,7 @@ public class EmsSdkClient(IEmsApi api) : IEmsSdkClient
 {
     public async Task<EmsReservationResponseModel> CreateReservationAsync(EmsReservationRequestModel requestModel)
     {
-        var result = await api.CreateProductAsync(requestModel.ToDto(), requestModel.Headers);
+        var result = await api.CreateReservationAsync(requestModel.ToDto(), requestModel.Headers);
         return result.ToModel();
     }
 }
--- EMSPackage.Sdk/Dto/IEmsApi.cs
+++ EMSPackage.Sdk/Dto/IEmsApi.cs
@@ -6,5 +6,5 @@ namespace EMSPackage.Sdk.Dto;
 public interface IEmsApi
 {
     [Post("/webhook/ReservationWebhook/CreateReservation")]
-    public Task<EmsCreateReservationResponseDto> CreateProductAsync(EmsCreateReservationRequestDto request, [HeaderCollection] IDictionary<string, string> headers);
+    public Task<EmsCreateReservationResponseDto> CreateReservationAsync(EmsCreateReservationRequestDto request, [HeaderCollection] IDictionary<string, string> headers);
 }
